59f345367c38737fbb0c6fa0aacd663b2e661581
NikhilWingsTech <nikhil.m@wingsts.com>
2022-03-23 13:36:05 +0530

    style and routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
       ]
   },
   {path: 'dash' , component: BoardComponent},
-  // {path: 'dash' , component: HeaderComponent},
+  // {path: 'header' , component: HeaderComponent},
 ];
 
 @NgModule({
--- client/src/app/user/user.module.ts
+++ client/src/app/user/user.module.ts
@@ -1,8 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatButtonModule } from '@angular/material/button';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from '../app-routing.module'
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
+
 import {RegisterComponent} from'./register/register.component';
 import {LoginComponent} from'./login/login.component';
 import { MainDeskComponent } from './main-desk/main-desk.component';
@@ -19,7 +25,13 @@ import { MainDeskComponent } from './main-desk/main-desk.component';
     FormsModule,
     ReactiveFormsModule,
     MatFormFieldModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    MatInputModule,
+    MatSnackBarModule,
+    MatButtonModule
+
   ],
   exports: [
     RegisterComponent,
